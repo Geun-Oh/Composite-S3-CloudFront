cbf54b580704fb867cf892fadf4830fca0d67774
Geun-Oh <kandy1002@naver.com>
2023-12-20 03:34:10 +0900

    release - v.0.4.1 : fixed bucket id

--- s3.tf
+++ s3.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_acl" "test" {
 resource "aws_s3_object" "test" {
   for_each = { for f in fileset("${path.module}/dist", "**/*") : f => f }
 
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.test.id
   key    = each.value
   source = "${path.module}/${var.filepath}/${each.value}"
   etag   = filemd5("${path.module}/${var.filepath}/${each.value}")
